045d720e7eb2c62616be4a2ca04ec30acd91826e
MerryHao <mq6721@gmail.com>
2022-12-12 20:16:30 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo') // 載入 Todo model
 
 //僅在非正式環境(production)時，使用dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -26,8 +27,12 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() //取出Todo model裡的所有資料
+    .lean() //把Mongoose的Model物件轉換成乾淨的JavaScript資料陣列
+    .then(todos => res.render('index', {todos})) //將樣板資料傳給index樣板
+    .catch(error => console.error(error)) //錯誤處理
 })
+
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
