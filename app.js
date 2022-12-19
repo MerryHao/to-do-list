4daf1865098cc2024a0542b488c148b555fd4d37
MerryHao <mq6721@gmail.com>
2022-12-19 17:42:07 +0800

    fix: mongoose connection

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo') // 載入 Todo model
+//const Todo = require('./models/todo') // 載入 Todo model
 const methodOverride = require('method-override') // 載入method-override
 const routes = require('./routes') // 引用路由器
-
-//僅在非正式環境(production)時，使用dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose')
+const dotenv = require("dotenv");
+// //僅在非正式環境(production)時，使用dotenv
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
 const app = express()
 const port = 3000
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
